a2a05a08d177c639eaf8e16006ecfbb5e270076c
iknowwherewallyis <jhandy92@gmail.com>
2019-02-21 15:17:14 +0000

    Update changeSecret.groovy

--- changeSecret.groovy
+++ changeSecret.groovy
@@ -1,8 +1,8 @@
-import jenkins.model.*
-import com.cloudbees.plugins.credentials.impl.*
-import com.cloudbees.plugins.*
+//import jenkins.model.*
+//import com.cloudbees.plugins.credentials.impl.*
+//import com.cloudbees.plugins.*
 import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
-import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl.DescriptorImpl;
+//import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl.DescriptorImpl;
 import hudson.util.Secret;
 
 
