722d7fb36182a8e94ae7598488fd3236053479d4
iknowwherewallyis <jhandy92@gmail.com>
2019-02-22 11:18:53 +0000

    Update changeSecret.groovy

--- changeSecret.groovy
+++ changeSecret.groovy
@@ -14,8 +14,8 @@ def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredenti
 def secret = Secret.fromString(new_secret)
 def c = creds.find {it.id == id}
 
-println "${c.scope}"
-/*
+//println "${c.scope}"
+
 if (!c) {
   println "could not find credential for ${id} in Jenkins credential store"
   return "Unable to pickup credential from Jenkins"
@@ -44,7 +44,7 @@ if ( c ) {
   }
 } else {
   println "could not find credential for ${c.id} in Jenkins credential store"
-*/
+}
 }
 return this;
 
