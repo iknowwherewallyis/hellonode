eab2f82266c6fb406a2b12e30a72a0c1759ccfa4
iknowwherewallyis <jhandy92@gmail.com>
2019-02-27 09:40:09 +0000

    Update changeSecret.groovy

--- changeSecret.groovy
+++ changeSecret.groovy
@@ -1,4 +1,3 @@
-import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 import hudson.util.Secret;
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.*
@@ -12,44 +11,21 @@ import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import com.cloudbees.plugins.credentials.domains.Domain;
 import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import com.cloudbees.plugins.credentials.domains.DomainSpecification;
-import com.cloudbees.plugins.credentials.domains.HostnameSpecification;
-import com.cloudbees.plugins.credentials.domains.SchemeSpecification;
-import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
 import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
-import hudson.Util;
-import hudson.model.AbstractProject;
-import hudson.model.Item;
-import hudson.model.Queue.Task;
-import hudson.model.queue.Tasks;
-import hudson.security.ACL;
-import hudson.util.ListBoxModel;
-import hudson.util.Secret;
 import jenkins.model.Jenkins;
 
 import org.acegisecurity.Authentication;
 import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 
-import javax.annotation.CheckForNull;
-import javax.inject.Singleton;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+
 
 import com.cloudbees.plugins.credentials.common.IdCredentials;
 import com.cloudbees.plugins.credentials.domains.DomainRequirement;
-import com.cloudbees.plugins.credentials.fingerprints.ItemCredentialsFingerprintFacet;
-import com.cloudbees.plugins.credentials.fingerprints.NodeCredentialsFingerprintFacet;
-import edu.umd.cs.findbugs.annotations.CheckForNull;
-import edu.umd.cs.findbugs.annotations.NonNull;
-import edu.umd.cs.findbugs.annotations.Nullable;
-import hudson.BulkChange;
-import hudson.DescriptorExtensionList;
-import hudson.ExtensionList;
-import hudson.ExtensionPoint;
-import hudson.Util;
-import hudson.init.InitMilestone;
+
 import hudson.model.Cause;
 import hudson.model.Computer;
 import hudson.model.ComputerSet;
@@ -68,57 +44,6 @@ import hudson.model.Queue;
 import hudson.model.Run;
 import hudson.model.User;
 import hudson.model.queue.Tasks;
-import hudson.security.ACL;
-import hudson.security.Permission;
-import hudson.security.PermissionGroup;
-import hudson.security.PermissionScope;
-import hudson.security.SecurityRealm;
-import hudson.util.ListBoxModel;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
-import java.security.DigestOutputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.text.Collator;
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import jenkins.model.FingerprintFacet;
-import jenkins.model.Jenkins;
-import jenkins.util.Timer;
-import org.acegisecurity.Authentication;
-import org.acegisecurity.GrantedAuthority;
-import org.acegisecurity.context.SecurityContext;
-import org.acegisecurity.context.SecurityContextHolder;
-import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
-import org.acegisecurity.userdetails.UsernameNotFoundException;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.output.NullOutputStream;
-import org.apache.commons.lang.StringUtils;
-import org.jenkins.ui.icon.IconSpec;
-import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.DoNotUse;
-import org.kohsuke.stapler.Stapler;
-import org.kohsuke.stapler.StaplerRequest;
-import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey
-import org.jenkinsci.plugins.plaincredentials.StringCredentials
-import com.cloudbees.plugins.credentials.*
-import com.cloudbees.plugins.credentials.impl.*
-import com.cloudbees.plugins.credentials.domains.*
 
 
 def changeSecretText(id, new_secret){
